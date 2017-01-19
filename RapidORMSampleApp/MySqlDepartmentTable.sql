a3de0253742c37614d9d23cd213135de3f07258f
Kent Aguilar <kentaguilar@gmail.com>
2017-01-19 11:14:09 +0800

    Added department table structure

---
+++ RapidORMSampleApp/MySqlDepartmentTable.sql
@@ -0,0 +1,8 @@
+﻿DROP TABLE IF EXISTS `department`;
+
+CREATE TABLE `department` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `date_created` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
